c8eff558a7badb703f61bdcc8be78f26df4944d0
inso <insofan3156@gmail.com>
2024-03-30 07:06:23 +0800

    first commit

---
+++ Start.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Start : Control
+{
+    // Called when the node enters the scene tree for the first time.
+    private Button _conn;
+
+    public override void _Ready()
+    {
+        _conn = GetNode<Button>("Conn");
+        _conn.Pressed += (() =>
+        {
+            Console.WriteLine("Hello 1");
+            SQLitePCL.Batteries.Init();
+            Console.WriteLine("Hello 2");
+        });
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
